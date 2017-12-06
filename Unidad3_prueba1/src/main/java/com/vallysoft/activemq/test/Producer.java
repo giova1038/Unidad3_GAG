d0a1628d9d317e78a528a4c1049503a95ce61f05
GIOVANNY <GIOVANNY@SOPORTE-GAG>
2017-12-05 20:38:37 -0500

    Actualizacion Actividad 3 Prueba2

--- Unidad3_prueba1/src/main/java/com/vallysoft/activemq/test/Producer.java
+++ Unidad3_prueba1/src/main/java/com/vallysoft/activemq/test/Producer.java
@@ -1,5 +1,8 @@
 package com.vallysoft.activemq.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -17,10 +20,11 @@ public class Producer {
 	private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
 	// default broker URL is : tcp://localhost:61616"
 
-	private static String subject = "VALLYSOFTQ"; // Queue Name
+	private static String subject = "LDS"; // Queue Name
 	// You can create any/many queue names as per your requirement.
 
 	public static void main(String[] args) throws JMSException {
+		int contQueue=100;
 		// Getting JMS connection from the server and starting it
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
 		Connection connection = connectionFactory.createConnection();
@@ -36,12 +40,30 @@ public class Producer {
 		// MessageProducer is used for sending messages (as opposed
 		// to MessageConsumer which is used for receiving them)
 		MessageProducer producer = session.createProducer(destination);
+		
 		// We will send a small text message saying 'Hello' in Japanese
-		TextMessage message = session.createTextMessage("Hello welcome come to vallysoft ActiveMQ!");
+		//TextMessage message = session.createTextMessage("Hello welcome come to LDS ActiveMQ!");
+		TextMessage[] arrayMessage = new TextMessage[contQueue];
+		//ArrayList listaMensajes = new ArrayList();
+		for(int i=0; i<contQueue; i++) {
+			arrayMessage[i]=session.createTextMessage("Hello welcome come to LDS ActiveMQ!"+i);
+			producer.send(arrayMessage[i]);
+			System.out.println("Sentage '" + arrayMessage [i].getText()+ "'");
+		}
+		
+//		for(int i=0; i<100; i++) {
+//			TextMessage message = session.createTextMessage("Hello welcome come to LDS ActiveMQ!" +i);
+//			listaMensajes.add(message);
+//			//producer.send(arrayMessage[i]);
+//			//System.out.println("Sentage '" + arrayMessage [i].getText()+ "'");
+//		}
+//		for(int i=0; i<listaMensajes.size(); i++) {
+//			producer.send(arrayMessage[i]);
+//			System.out.println("Sentage '" + arrayMessage [i].getText()+ "'");
+//		}
 		// Here we are sending the message!
-		producer.send(message);
-		System.out.println("Sentage '" + message.getText() + "'");
-
+		//producer.send(message);
+		//System.out.println("Sentage '" + Message.getText() + "'");
 		connection.close();
 	}
 
